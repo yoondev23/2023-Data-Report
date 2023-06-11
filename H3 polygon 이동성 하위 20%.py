2958f871afc5c73137848caa428f73bc0f8fbccd
yoon.dev23 <128372198+yoondev23@users.noreply.github.com>
2023-06-11 20:26:53 +0900

    H3 polygon move conv score

---
+++ H3 polygon 이동성 하위 20%.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from IPython.display import display, HTML
+display(HTML("<style>.container { width:80% !important; }</style>"))
+
+
+# In[3]:
+
+
+import pandas as pd
+import geopandas as gpd
+import folium 
+import json
+import pyproj
+from pyproj import Proj, transform,Transformer
+from shapely.geometry import Point
+from shapely.ops import transform
+from functools import partial
+
+
+# In[4]:
+
+
+#shp 파일에서 불러오기
+
+sd_busan = gpd.read_file('./SD/ctp_rvn.shp', encoding='euc-kr')
+sd_busan = sd_busan[sd_busan.CTP_KOR_NM.str.contains('부산')].geometry[1]
+
+
+# In[5]:
+
+
+#epsg 변경
+project = pyproj.Transformer.from_proj('EPSG:5179', 'EPSG:4326', always_xy=True).transform
+ 
+sd_busan = transform(project, sd_busan)
+
+
+# In[ ]:
+
+
+# folium map
+lat = sd_busan.centroid.y
+lng = sd_busan.centroid.x
+location = [lat-0.04, lng]
+
+
+# In[54]:
+
+
+# 기본맵
+m = folium.Map(
+    location=location,
+    zoom_start=10, 
+    tiles= "CartoDB positron", #"Stamen Terrain  CartoDB positron
+)
+
+
+# In[55]:
+
+
+# 부산지역
+geo_poly = folium.GeoJson(data=sd_busan, style_function=lambda x: {'line_weight' : 0.004,'fillColor': 'red', 'fillOpacity': 0.1})
+geo_poly.add_to(m)
+
+m
+
+
+# # 기준치 : 점수 분포 20분위
+
+# In[19]:
+
+
+h3_conv_score = pd.read_csv('h3_conv_score.csv')
+
+# 기준치
+th_mov_h3 = h3_conv_score.move_conv_score_norm.quantile(q=0.2)
+
+
+# In[20]:
+
+
+h3_conv_score['geometry'] = gpd.GeoSeries.from_wkt(h3_conv_score['geometry'])
+
+
+# In[56]:
+
+
+# 이동편의성 하위 20%
+intersect_poly_lst_move = h3_conv_score.loc[(h3_conv_score.move_conv_score_norm < th_mov_h3), ['geometry']]['geometry']
+
+for polygon in intersect_poly_lst_move:
+    geo = gpd.GeoSeries(polygon)
+    polygon_json = geo.to_json()
+    geo_poly = folium.GeoJson(data=polygon_json, style_function=lambda x: {'fillColor': '#FFBF00', 'fillOpacity': 0.4,'lineColor': '#3A2F0B'})
+    geo_poly.add_to(m)
+    
+m    
+
+
+# In[28]:
+
+
+# 이동편의성 하위 20%의 중심점 출력
+intersect_poly_lst_move = h3_conv_score.loc[(h3_conv_score.move_conv_score_norm < th_mov_h3), ['geometry']]['geometry']
+intersect_poly_lst_move 
+
+
+# In[ ]:
+
+
+
+
